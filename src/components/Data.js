0a2d691a75526f0a1a89fa45565e6dd50a0d1e82
Rahul Gupta <guptaji048@gmail.com>
2021-04-19 11:06:14 +0530

    Category image data array

---
+++ src/components/Data.js
@@ -0,0 +1,12 @@
+const categoryPhotosArr = [
+    'https://res.cloudinary.com/guptaji048/image/upload/v1618758819/south-indian_ehrhwx.webp',
+    'https://res.cloudinary.com/guptaji048/image/upload/v1618806958/men_s-wear_qoxao7.jpg',
+    'https://res.cloudinary.com/guptaji048/image/upload/v1618807251/beverages_t8mn51.jpg',
+    'https://res.cloudinary.com/guptaji048/image/upload/v1618808442/electronic-gadgets_jezncr.jpg',
+    'https://res.cloudinary.com/guptaji048/image/upload/v1618808870/baby-care_q0kgur.jpg',
+    'https://res.cloudinary.com/guptaji048/image/upload/v1618808975/grocery_rceg30.png',
+    'https://res.cloudinary.com/guptaji048/image/upload/v1618809450/vegetables_cc4mni.jpg',
+    'https://res.cloudinary.com/guptaji048/image/upload/v1618809554/fruits_jem9f8.jpg',
+];
+
+export default categoryPhotosArr;
