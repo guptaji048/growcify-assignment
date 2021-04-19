7f38dc56a50a8a0c371e07976c04318ad27db87a
Rahul Gupta <guptaji048@gmail.com>
2021-04-19 14:04:26 +0530

    Home component added

---
+++ src/components/CategoryHome.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Typography, Divider, makeStyles } from '@material-ui/core';
+import CategoryList from "./CategoryList";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '70%',
+    border: '1px solid #D3D3D3',
+    borderRadius: 5,
+  },
+  header: {
+    backgroundColor: '#007bff',
+    color: 'white',
+    padding: 12,
+    boxShadow: theme.shadows[2],
+  },
+}));
+
+export default function CategoryHome() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.header}>
+        <Typography variant="h4" className={classes.heading}>Categories</Typography>
+      </div>
+      <Divider />
+      <CategoryList />
+    </div>
+  );
+}
