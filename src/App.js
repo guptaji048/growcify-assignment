a209e1386e39dbdc952f8554ea4856f87ba2fb21
Rahul Gupta <guptaji048@gmail.com>
2021-04-19 14:07:19 +0530

    Context added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import CategoryHome from "./components/CategoryHome";
 import CategoryItem from "./components/CategoryItem";
+import { CategoryProvider } from "./contexts/CategoryContext";
 
 export default function App() {
   return (
     <Router>
-       <Switch>
-         <Route exact path="/" component={CategoryHome} />
-         <Route path="/category/:id" component={CategoryItem} />
-       </Switch>
+      <CategoryProvider>
+        <Switch>
+          <Route exact path="/" component={CategoryHome} />
+          <Route path="/category/:id" component={CategoryItem} />
+        </Switch>
+      </CategoryProvider>
     </Router>
   );
 }
