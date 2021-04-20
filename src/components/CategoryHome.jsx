f6379141d755713a2d01ee6faeaf386ca8f5d31b
Rahul Gupta <guptaji048@gmail.com>
2021-04-20 11:05:09 +0530

    Container added

--- src/components/CategoryHome.jsx
+++ src/components/CategoryHome.jsx
@@ -1,16 +1,12 @@
 import React from "react";
-import { Typography, Divider, makeStyles } from '@material-ui/core';
+import { Typography, Divider, makeStyles, Container } from '@material-ui/core';
 import CategoryList from "./CategoryList";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: '70%',
     border: '1px solid #D3D3D3',
     borderRadius: 5,
+    padding: 0,
   },
   header: {
     backgroundColor: '#007bff',
@@ -24,12 +20,12 @@ export default function CategoryHome() {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
+    <Container maxWidth="md" className={classes.root}>
       <div className={classes.header}>
         <Typography variant="h4" className={classes.heading}>Categories</Typography>
       </div>
       <Divider />
       <CategoryList />
-    </div>
+    </Container>
   );
 }
