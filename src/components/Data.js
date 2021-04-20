a8c4fe4278184867c4ca6a54dca4d2a4c2b1d61e
Rahul Gupta <guptaji048@gmail.com>
2021-04-20 11:12:01 +0530

    Dummy image data added

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,4 +1,4 @@
-const categoryPhotosArr = [
+export const categoryPhotosArr = [
     'https://res.cloudinary.com/guptaji048/image/upload/v1618758819/south-indian_ehrhwx.webp',
     'https://res.cloudinary.com/guptaji048/image/upload/v1618806958/men_s-wear_qoxao7.jpg',
     'https://res.cloudinary.com/guptaji048/image/upload/v1618807251/beverages_t8mn51.jpg',
@@ -9,4 +9,31 @@ const categoryPhotosArr = [
     'https://res.cloudinary.com/guptaji048/image/upload/v1618809554/fruits_jem9f8.jpg',
 ];
 
-export default categoryPhotosArr;
+export const southIndian = [
+    'https://res.cloudinary.com/guptaji048/image/upload/v1618841218/onion-dosa_jfou2c.png',
+    'https://res.cloudinary.com/guptaji048/image/upload/v1618840458/masala-dosa_yqyrds.webp',
+];
+
+export const tshirt = ['https://res.cloudinary.com/guptaji048/image/upload/v1618852456/tshirt_b1ocs9.webp'];
+
+export const beverage = ['https://res.cloudinary.com/guptaji048/image/upload/v1618852636/mazaa_x3plzf.jpg'];
+
+export const grocery = [
+    'https://res.cloudinary.com/guptaji048/image/upload/v1618852888/oil_fb2iuk.jpg',
+    'https://res.cloudinary.com/guptaji048/image/upload/v1618852899/dal_kl4hzw.jpg',
+    'https://res.cloudinary.com/guptaji048/image/upload/v1618852887/rice_a1maix.jpg',
+];
+
+export const fruits = [
+    'https://res.cloudinary.com/guptaji048/image/upload/v1618854258/grapes_akfhgi.jpg',
+    'https://res.cloudinary.com/guptaji048/image/upload/v1618854105/bananas_fsquw3.webp',
+    'https://res.cloudinary.com/guptaji048/image/upload/v1618853349/mango_s3s011.jpg',
+    'https://res.cloudinary.com/guptaji048/image/upload/v1618854105/bananas_fsquw3.webp',
+    'https://res.cloudinary.com/guptaji048/image/upload/v1618853349/mango_s3s011.jpg',
+];
+
+export const vegetables = [
+    'https://res.cloudinary.com/guptaji048/image/upload/v1618854451/pumpkin_fspssj.jpg',
+    'https://res.cloudinary.com/guptaji048/image/upload/v1618853354/cauliflower_eyj54l.png',
+    'https://res.cloudinary.com/guptaji048/image/upload/v1618853355/lady-finger_lcrqfu.jpg',
+];
